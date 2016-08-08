cbbd67ec0d22762ac609bc98671a0521022772ee
mi_rafi <rafi@workspaceit.com>
2016-08-08 12:22:31 +0600

    Minor refactor

--- src/model/entity/app/Product.java
+++ src/model/entity/app/Product.java
@@ -2,39 +2,26 @@ package model.entity.app;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-import java.util.Collection;
 
 /**
- * Created by mi on 8/1/16.
+ * Created by mi on 8/8/16.
  */
 @Entity
 @Table(name = "product", schema = "", catalog = "rentguru24")
 public class Product {
     private int id;
-    private int ownerId;
     private String name;
     private String description;
-    private String images;
-    private double currentValue;
-    private double fee;
-    private boolean available;
-    private boolean reviewStatus;
     private String profileImage;
     private String otherImages;
+    private double currentValue;
     private double rentFee;
     private boolean active;
     private boolean currentlyAvailable;
     private Timestamp availableFrom;
     private Timestamp availableTill;
+    private boolean reviewStatus;
     private Timestamp createdDate;
-
-
-    //private Collection<ProductAttribute> productAttributes;
-
-
-    public void setReviewStatus(boolean reviewStatus) {
-        this.reviewStatus = reviewStatus;
-    }
 
     @Id
     @Column(name = "id")
@@ -44,16 +31,6 @@ public class Product {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    @Basic
-    @Column(name = "owner_id")
-    public int getOwnerId() {
-        return ownerId;
-    }
-
-    public void setOwnerId(int ownerId) {
-        this.ownerId = ownerId;
     }
 
     @Basic
@@ -76,106 +53,6 @@ public class Product {
         this.description = description;
     }
 
-
-
-    public void setImages(String images) {
-        this.images = images;
-    }
-
-    @Basic
-    @Column(name = "current_value")
-    public double getCurrentValue() {
-        return currentValue;
-    }
-
-    public void setCurrentValue(double currentValue) {
-        this.currentValue = currentValue;
-    }
-
-    @Basic
-    @Column(name = "fee")
-    public double getFee() {
-        return fee;
-    }
-
-    public void setFee(double fee) {
-        this.fee = fee;
-    }
-
-    @Basic
-    @Column(name = "available")
-    public boolean isAvailable() {
-        return available;
-    }
-
-//    @OneToMany(mappedBy = "productByProductId")
-//    public Collection<ProductAttribute> getProductAttributes() {
-//        return productAttributes;
-//    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
-    @Basic
-    @Column(name = "review_status")
-    public boolean isReviewStatus() {
-        return reviewStatus;
-    }
-
-
-
-    @Basic
-    @Column(name = "created_date")
-    public Timestamp getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Timestamp createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Product that = (Product) o;
-
-        if (id != that.id) return false;
-        if (ownerId != that.ownerId) return false;
-        if (Double.compare(that.currentValue, currentValue) != 0) return false;
-        if (Double.compare(that.fee, fee) != 0) return false;
-        if (available != that.available) return false;
-        if (reviewStatus != that.reviewStatus) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-        if (description != null ? !description.equals(that.description) : that.description != null) return false;
-        if (images != null ? !images.equals(that.images) : that.images != null) return false;
-        if (createdDate != null ? !createdDate.equals(that.createdDate) : that.createdDate != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = id;
-        result = 31 * result + ownerId;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + (images != null ? images.hashCode() : 0);
-        temp = Double.doubleToLongBits(currentValue);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(fee);
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + (available ? 1 : 0);
-        result = 31 * result + (reviewStatus ? 1 : 0);
-        result = 31 * result + (createdDate != null ? createdDate.hashCode() : 0);
-        return result;
-    }
-
-
     @Basic
     @Column(name = "profile_image")
     public String getProfileImage() {
@@ -194,6 +71,16 @@ public class Product {
 
     public void setOtherImages(String otherImages) {
         this.otherImages = otherImages;
+    }
+
+    @Basic
+    @Column(name = "current_value")
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
     }
 
     @Basic
@@ -244,5 +131,73 @@ public class Product {
 
     public void setAvailableTill(Timestamp availableTill) {
         this.availableTill = availableTill;
+    }
+
+    @Basic
+    @Column(name = "review_status")
+    public boolean isReviewStatus() {
+        return reviewStatus;
+    }
+
+    public void setReviewStatus(boolean reviewStatus) {
+        this.reviewStatus = reviewStatus;
+    }
+
+    @Basic
+    @Column(name = "created_date")
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product that = (Product) o;
+
+        if (id != that.id) return false;
+        if (Double.compare(that.currentValue, currentValue) != 0) return false;
+        if (Double.compare(that.rentFee, rentFee) != 0) return false;
+        if (active != that.active) return false;
+        if (currentlyAvailable != that.currentlyAvailable) return false;
+        if (reviewStatus != that.reviewStatus) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (profileImage != null ? !profileImage.equals(that.profileImage) : that.profileImage != null) return false;
+        if (otherImages != null ? !otherImages.equals(that.otherImages) : that.otherImages != null) return false;
+        if (availableFrom != null ? !availableFrom.equals(that.availableFrom) : that.availableFrom != null)
+            return false;
+        if (availableTill != null ? !availableTill.equals(that.availableTill) : that.availableTill != null)
+            return false;
+        if (createdDate != null ? !createdDate.equals(that.createdDate) : that.createdDate != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (profileImage != null ? profileImage.hashCode() : 0);
+        result = 31 * result + (otherImages != null ? otherImages.hashCode() : 0);
+        temp = Double.doubleToLongBits(currentValue);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(rentFee);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (active ? 1 : 0);
+        result = 31 * result + (currentlyAvailable ? 1 : 0);
+        result = 31 * result + (availableFrom != null ? availableFrom.hashCode() : 0);
+        result = 31 * result + (availableTill != null ? availableTill.hashCode() : 0);
+        result = 31 * result + (reviewStatus ? 1 : 0);
+        result = 31 * result + (createdDate != null ? createdDate.hashCode() : 0);
+        return result;
     }
 }
