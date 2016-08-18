bf71f03f0a7019d66d35d05fbe81fdd5e10b2a36
zakariya_naseef <zakariya.omar.naseef@gmail.com>
2016-08-18 11:06:29 +0600

    upload product

--- src/controller/web/app/ProductController.java
+++ src/controller/web/app/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
