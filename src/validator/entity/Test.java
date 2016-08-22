5802b2f842a504a70e99bf55b0026cc751ecc748
mi_rafi <rafi@workspaceit.com>
2016-08-22 18:24:37 +0600

    RetalModel refactor

--- src/validator/entity/Test.java
+++ src/validator/entity/Test.java
@@ -1,7 +1,13 @@
 package validator.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.IOException;
 
 public class Test {
     public static void main(String args[]){
@@ -23,21 +29,53 @@ public class Test {
 }
 
 class A implements I,J{
+
     @Override
+    @JsonSerialize(using=ISerializer.class)
     public String getABC() {
         return "ABC";
     }
 
     @Override
+    @JsonSerialize(using=JSerializer.class)
     public String getEFG() {
         return "EFG";
+    }
+}
+class B implements I{
+    @Override
+    public String getABC() {
+        return null;
     }
 }
 
 interface I{
     String getABC();
 }
-@JsonSerialize(as=J.class)
+
 interface J extends I{
     String getEFG();
+}
+
+class ISerializer extends JsonSerializer<I> {
+    @Override
+    public void serialize(I value,
+                          JsonGenerator jgen,
+                          SerializerProvider provider) throws IOException {
+
+        jgen.writeStartObject();
+        jgen.writeStringField("abc", value.getABC());
+        jgen.writeEndObject();
+    }
+}
+class JSerializer extends JsonSerializer<J> {
+    @Override
+    public void serialize(J value,
+                          JsonGenerator jgen,
+                          SerializerProvider provider) throws IOException {
+
+        jgen.writeStartObject();
+        jgen.writeStringField("efg", value.getABC());
+        jgen.writeEndObject();
+    }
 }
