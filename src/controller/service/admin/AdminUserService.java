e56532cf795f610ad8db8c6283a9233ed5056ba3
mi_rafi <rafi@workspaceit.com>
2016-08-24 17:18:35 +0600

    Conflict resolved

--- src/controller/service/admin/AdminUserService.java
+++ src/controller/service/admin/AdminUserService.java
@@ -1,8 +1,6 @@
 package controller.service.admin;
 
 
-<<<<<<< HEAD
-=======
 import helper.ServiceResponse;
 import model.AppLoginCredentialModel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
->>>>>>> 005e7e06340ec0db489547718f1d5916414f9970
 
 /**
  * Created by omar on 8/24/16.
  */
-<<<<<<< HEAD
 
-public class AdminUserService {
 
-=======
 @RestController
 @RequestMapping("/api-admin")
 public class AdminUserService {
@@ -38,5 +32,4 @@ public class AdminUserService {
         appLoginCredentialModel.appUserStatusUpdate(appUserId,varified);
         return serviceResponse;
     }
->>>>>>> 005e7e06340ec0db489547718f1d5916414f9970
 }
