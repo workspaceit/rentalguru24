f0d966182ae82633feda24fe01c8ddf2d8de8e4e
mi_rafi <rafi@workspaceit.com>
2016-08-24 12:43:53 +0600

    Interceptor added

---
+++ src/model/ProductReturnedModel.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by mi on 8/24/16.
+ */
+public class ProductReturnedModel extends BaseModel {
+}
--- src/model/RentProductModel.java
+++ src/model/RentProductModel.java
@@ -28,7 +28,7 @@ public class RentProductModel extends BaseModel {
         try{
             session = this.sessionFactory.openSession();
             String hql = "from RentProduct where ( ( startDate BETWEEN :startDate and :endsDate ) or" +
-                    " ( endsDate BETWEEN :startDate and :endsDate ) ) and rentalProduct.id = :productId ";
+                    " ( endsDate BETWEEN :startDate and :endsDate ) ) and rentalProduct.id = :productId and expired = false ";
 
             Query query =  session.createQuery(hql);
 
